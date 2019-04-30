70f98e97fb76d30f4b62cbae44f47fdd5db07c3e
Adrian Göransson <adriangoransson@gmail.com>
2019-04-30 11:01:03 +0200

    Show important details, blueprint and satellite view in apt details

--- src/components/Apartment.vue
+++ src/components/Apartment.vue
@@ -7,11 +7,62 @@
       <br>
       {{ data.area }}, {{ data.address }}
     </div>
-    <div v-if="extendedInfo && details.productId">
-      <pre>{{ details }}</pre>
-      <a :href="blueprintURL">
-        <img :src="blueprintURL" alt="Ritning">
+    <div v-if="extendedInfo && details.productId" class="details">
+      <a target="_blank" :href="`https://www.afbostader.se/lediga-bostader/bostadsdetalj/?obj=${data.productId}`">
+        Öppna hos AFB
       </a>
+
+      <p>
+        <table>
+          <tr>
+            <th>Möbler</th>
+            <td>{{ details.furniture }}</td>
+          </tr>
+
+          <tr>
+            <th>Kök</th>
+            <td>{{ details.citchen }}</td>
+          </tr>
+
+          <tr>
+            <th>Balkong/Uteplats</th>
+            <td>{{ details.balkony }}</td>
+          </tr>
+
+          <tr>
+            <th>Hiss</th>
+            <td>{{ details.elevator }}</td>
+          </tr>
+
+          <tr>
+            <th>Värme/Vatten</th>
+            <td>{{ details.heating }}</td>
+          </tr>
+
+          <tr>
+            <th>El</th>
+            <td>{{ details.electricity }}</td>
+          </tr>
+
+          <tr>
+            <th>Förråd</th>
+            <td>{{ details.storeIncluded }}</td>
+          </tr>
+
+          <tr>
+            <th>Läge</th>
+            <td>{{ details.location }}</td>
+          </tr>
+        </table>
+      </p>
+
+      <p>{{ details.descriptionText }}</p>
+
+      <p><a target="_blank" :href="blueprintURL"><img :src="blueprintURL" alt="Ritning"></a></p>
+
+      <div class="maps">
+        <img :src="mapsURL" alt="Satellite view">
+      </div>
     </div>
   </div>
 </template>
@@ -56,6 +107,11 @@ export default {
     blueprintURL() {
       return `https://www.afbostader.se${this.details.blueprint}`;
     },
+
+    mapsURL() {
+      const address = encodeURIComponent(`${this.details.addressgroup}, ${this.details.zipcode} ${this.details.city}`);
+      return `https://maps.google.com/maps/api/staticmap?center=${address}&zoom=17&size=440x300&maptype=satellite&markers=color:red%7Clabel:S%7C${address}&key=AIzaSyCbmn5bUDuTStyK9i09MDR5T2mVAzUNJLI`;
+    },
   },
 
   methods: {
@@ -84,8 +140,26 @@ export default {
     cursor: pointer;
   }
 
+  .outer .details {
+    padding: 10px;
+    background: hsl(0, 0%, 95%);
+  }
+
   .tooltip {
     text-decoration: underline dotted;
     cursor: help;
+  }
+
+  .maps {
+    overflow-x: scroll;
+  }
+
+  tr {
+    margin-top: 3px;
+  }
+
+  th {
+    text-align: left;
+    padding-right: 15px;
   }
 </style>
